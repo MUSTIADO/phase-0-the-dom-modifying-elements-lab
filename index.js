a4b84a841a96cb6cb09b6076d76b9ad94d2cf17e
MUSTIADO <farhanmaalim6@gmail.com>
2024-04-03 12:25:05 +0300

    feat:phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Remove the DOM node with id 'main#main'
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element
+const yourName = 'MUSTAFA'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append the new <h1> element to the document body or any desired location
+document.body.appendChild(newHeader);
